b4792dfb0ff38f103a59a777837a5adab214149e
Reivax4012 <xavier.huber@epitech.eu>
2022-04-10 21:55:24 +0200

    added criterion test basique

---
+++ tests/test_criterion.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2019
+** test
+** File description:
+** delivery
+*/
+
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+
+Test(bsq, test1)
+{
+    cr_assert_eq(84, 84);
+}
+
+Test(bsq, test2)
+{
+    cr_assert_eq(84, 84);
+}
